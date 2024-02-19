71d2b78c797ffd5d95c6113f536c19828472c60a
Danielton Paiva <53627217+danieltonpaiva@users.noreply.github.com>
2024-02-19 20:27:45 -0300

    dsfef

--- facefusion/uis/components/source.py
+++ facefusion/uis/components/source.py
@@ -34,10 +34,15 @@ def render() -> None:
 		show_label = False
 	)
 	register_ui_component('source_image', SOURCE_IMAGE)
-	arquivos = [f for f in os.listdir('/content/facenico5/exemplos') if os.path.isfile(os.path.join('/content/facenico5/exemplos', f))]
+	caminho = ''
+	if(facefusion.globals.isCollab):
+		caminho = '/content/facenico5/exemplos'
+	else:
+		caminho = '/kaggle/working/facenico6/exemplos'
+	arquivos = [f for f in os.listdir(caminho) if os.path.isfile(os.path.join(caminho, f))]
 	files = []
 	for x in arquivos:
-		files.append('/content/facenico5/exemplos/' + x)
+		files.append(caminho + '/' + x)
 
 	examples = gradio.Examples(sorted(files), SOURCE_FILE, examples_per_page=20)
 
