01a99ed6ed1dc00363383989a79d820c82d6be4f
Danielton Paiva <53627217+danieltonpaiva@users.noreply.github.com>
2024-02-24 19:01:03 -0300

    Update default.py

--- facefusion/uis/layouts/default.py
+++ facefusion/uis/layouts/default.py
@@ -74,4 +74,4 @@ def listen() -> None:
 
 
 def run(ui : gradio.Blocks) -> None:
-	ui.launch(inline = False, prevent_thread_lock=True, share=facefusion.globals.isCollab, debug=True)
+	ui.launch(show_api=False, share=facefusion.globals.isCollab)
