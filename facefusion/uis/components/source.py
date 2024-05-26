a6bc7f124c391b6b7ec8f62aae5824a00ab77f6f
Danielton Paiva <53627217+danieltonpaiva@users.noreply.github.com>
2024-05-26 19:31:03 -0300

    Update source.py

--- facefusion/uis/components/source.py
+++ facefusion/uis/components/source.py
@@ -44,7 +44,7 @@ def render() -> None:
 	for x in arquivos:
 		files.append(caminho + '/' + x)
 
-	examples = gradio.Examples(examples=sorted(files), inputs=SOURCE_FILE, outputs=SOURCE_IMAGE, examples_per_page=20)
+	examples = gradio.Examples(examples=sorted(files), inputs=SOURCE_FILE, outputs=SOURCE_IMAGE,fn=update, examples_per_page=20)
 
 
 def listen() -> None:
